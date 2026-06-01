85955b3f356a63736177bea5c8f34da4851558f7
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-06-01 16:42:17 -0400

    Websocket connection test passes

--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,31 @@
-import { io } from 'socket.io-client';
-import { expect, test } from 'vitest';
+import { io as clientIO } from 'socket.io-client';
+import { expect, test, beforeAll, afterAll } from 'vitest';
+import { httpServer } from './index.js';
 
 let clientSocket;
+const TEST_PORT = 9999;
+
+beforeAll(() => {
+  return new Promise((resolve) => {
+    httpServer.listen(TEST_PORT, () => {
+      resolve();
+    });
+  });
+});
+
+afterAll(() => {
+  if (clientSocket && clientSocket.connected) {
+    clientSocket.disconnect();
+  }
+  return new Promise((resolve) => {
+    httpServer.close(() => {
+      resolve();
+    });
+  });
+});
 
 test('should establish a real-time websocket connection', () => {
-  clientSocket = io('http://localhost:9999');
+  clientSocket = clientIO(`http://localhost:${TEST_PORT}`);
   
   return new Promise((resolve, reject) => {
     clientSocket.on('connect', () => {
@@ -12,8 +33,12 @@ test('should establish a real-time websocket connection', () => {
       resolve();
     });
     
+    clientSocket.on('connect_error', (err) => {
+      reject(err);
+    });
+    
     setTimeout(() => {
-      reject(new Error('Connection timed out as expected for failing step!'));
+      reject(new Error('Connection timed out!'));
     }, 1000);
   });
 });
