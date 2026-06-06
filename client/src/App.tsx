abee0d84872e2b3cae38da9ab14018f9c2bf38d1
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-06-06 16:36:47 -0400

    Changed Title To Global Constant

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { GameCanvas } from './components/GameCanvas.tsx'
+import { GAME_TITLE } from '../../shared/constants'
 import './App.css'
 
 function App() {
   return (
     <div className="app-viewport">
-      <div className="title-overlay">TopDown Tactix</div>
+      <div className="title-overlay">{GAME_TITLE}</div>
       <GameCanvas />
     </div>
   )
