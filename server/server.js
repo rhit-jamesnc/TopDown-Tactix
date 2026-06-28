ce5b3f338cb3027bf140e93f5d51c2f4c66da8b5
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-06-28 14:19:40 -0400

    Added Server Reset of Pending States, Dropped Incoming Movement Vectors While Paused, Cleaned Up State Flags When Game Hits Game-Over Condition

--- server/server.js
+++ server/server.js
@@ -90,7 +90,7 @@ io.on('connection', (socket) => {
     const roomId = playerToRoom.get(data.id);
     const gameData = games.get(roomId);
 
-    if (gameData && GamePhysicsEngine.isValidMove(data.move)) {
+    if (gameData & !gameData.instance.isPaused && GamePhysicsEngine.isValidMove(data.move)) {
       const player = gameData.instance.players[data.id];
       if (player) {
         Matter.Body.applyForce(player, player.position, data.move);
@@ -112,7 +112,7 @@ io.on('connection', (socket) => {
           io.to(roomId).emit('game-paused', true);
           return;
         }
-        
+
         if (!gameData.instance.isPaused) {
           gameData.instance.isPausePending = true;
           gameData.instance.pauseRequestedBy = socket.id; 
@@ -146,6 +146,10 @@ io.on('connection', (socket) => {
 
     if (gameData) {
       clearInterval(gameData.instance.loop);
+
+      gameData.instance.isPaused = false;
+      gameData.instance.isPausePending = false;
+      gameData.instance.pauseRequestedBy = null;
       
       gameData.players.forEach(pid => playerToRoom.delete(pid));
       games.delete(roomId);
@@ -208,6 +212,13 @@ export const startNewGame = (player1Id, player2Id) => {
     const status = newGame.getGameStatus();
     if (status !== 'ongoing') {
         clearInterval(loop);
+
+        newGame.isPaused = false;
+        newGame.isPausePending = false;
+        newGame.pauseRequestedBy = null;
+        io.to(roomId).emit('pause-pending', { pending: false, requestedBy: null });
+        io.to(roomId).emit('game-paused', false);
+        
         io.to(roomId).emit('game-over', {
             ...status,
             players: {
