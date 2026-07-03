4de8f4f8c1a9354b33eded7bafeaee56813c6271
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-07-03 18:12:59 -0400

    Added Default Route To HomePage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.tsx';
 import { AdminDashboardPage } from './Pages/AdminDashboardPage/AdminDashboardPage.tsx';
 import { inject } from '@vercel/analytics';
@@ -14,6 +14,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<AdminDashboardPage />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
