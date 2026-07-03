f50790bae6eaf42ebb77ab1683c58e5a0c841441
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-07-02 22:43:54 -0400

    Added Fields To Report Bug Modal

--- client/src/components/Modals/BugReportModal/BugReport.tsx
+++ client/src/components/Modals/BugReportModal/BugReport.tsx
@@ -1,11 +1,24 @@
-import './BugReportModal.css'
+import './BugReportModal.css';
 
 export const BugReportModal = ({ onClose }: { onClose: () => void }) => {
     return (
         <div className="bug-modal-overlay">
             <div className="bug-modal-content">
                 <h2>Report a Bug</h2>
-                <button onClick={onClose}>Close</button>
+                <form>
+                    <label>
+                        Email:
+                        <input type="email" required />
+                    </label>
+                    <label>
+                        Description:
+                        <textarea required />
+                    </label>
+                    <div className="button-group">
+                        <button type="button" onClick={onClose}>Cancel</button>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
