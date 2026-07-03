567ec98c4f75c4371cd8143cab2e257b0ed3efa9
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-07-02 21:27:41 -0400

    Added Dynamic Last Updated Text

--- client/src/components/Modals/SideMenu/SideMenu.tsx
+++ client/src/components/Modals/SideMenu/SideMenu.tsx
@@ -1,24 +1,31 @@
+import { useEffect, useState } from 'react';
 import './SideMenu.css';
-
-interface SideMenuProps {
-    isOpen: boolean;
-}
+import type { SideMenuProps } from '../../../../../shared/types/props';
 
 export const SideMenu = ({ isOpen }: SideMenuProps) => {
-  return (
-    <div className={`side-menu ${isOpen ? 'open' : ''}`}>
-      <nav className="menu-items">
-        <button>Admin Board</button>
-        <button>Report a Bug</button>
-        <button onClick={() => window.open('https://github.com/rhit-jamesnc/TopDown-Tactix', '_blank')}>
-          GitHub Repo
-        </button>
-      </nav>
+    const [lastUpdated, setLastUpdated] = useState('Loading...');
 
-      <footer className="menu-footer">
-        <p>Created by: Noah James</p>
-        <p>Last Updated: June 2026</p>
-      </footer>
-    </div>
-  );
+    useEffect(() => {
+        fetch('/version.json')
+        .then((res) => res.json())
+        .then((data) => setLastUpdated(data.lastUpdated))
+        .catch(() => setLastUpdated('July 02, 2026')); // Fallback
+    }, []);
+
+    return (
+        <div className={`side-menu ${isOpen ? 'open' : ''}`}>
+        <nav className="menu-items">
+            <button>Admin Board</button>
+            <button>Report a Bug</button>
+            <button onClick={() => window.open('https://github.com/rhit-jamesnc/TopDown-Tactix', '_blank')}>
+            GitHub Repo
+            </button>
+        </nav>
+
+        <footer className="menu-footer">
+            <p>Created by: Noah James</p>
+            <p>Last Updated: {lastUpdated}</p>
+        </footer>
+        </div>
+    );
 };
