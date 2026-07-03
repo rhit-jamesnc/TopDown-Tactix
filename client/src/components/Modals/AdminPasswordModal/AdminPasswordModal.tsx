20422c96d8ff4cd9c1f723e18212511a9a873e9c
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-07-03 17:25:40 -0400

    Incorrect Pssword Resets Password Value

--- client/src/components/Modals/AdminPasswordModal/AdminPasswordModal.tsx
+++ client/src/components/Modals/AdminPasswordModal/AdminPasswordModal.tsx
@@ -3,10 +3,22 @@ import './AdminPasswordModal.css';
 
 export const AdminPasswordModal = ({ onVerify, onClose }: { onVerify: (password: string) => void, onClose: () => void }) => {
   const [password, setPassword] = useState('');
+
+  const handleEnter = () => {
+    onVerify(password);
+    setPassword('');
+  };
+
   return (
     <div className="admin-password-modal">
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
-      <button onClick={() => onVerify(password)}>Enter</button>
+      <input 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+        placeholder="Enter password" 
+      />
+
+      <button onClick={handleEnter}>Enter</button>
       <button onClick={onClose}>Cancel</button>
     </div>
   );
