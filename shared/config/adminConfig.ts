17f3a6a8ae686dd8567e2b7c5cd32ace0a357abe
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-07-04 15:27:09 -0400

    Moved Admin Passwords To .env File

--- shared/config/adminConfig.ts
+++ shared/config/adminConfig.ts
@@ -1,7 +1,14 @@
+const OWNER_PASS = process.env.REACT_APP_OWNER_PASSWORD;
+const ADMIN_PASS = process.env.REACT_APP_ADMIN_PASSWORD;
+
+if (!OWNER_PASS || !ADMIN_PASS) {
+  throw new Error("Missing required environment variables for Admin configuration!");
+}
+
 export const ADMIN_CONFIG = {
   PASSWORDS: {
-    OWNER: 'OWNER_PASSWORD',
-    ADMIN: 'ADMIN_PASSWORD',
+    OWNER: OWNER_PASS,
+    ADMIN: ADMIN_PASS,
   },
   TYPES: {
     OWNER: 'owner',
