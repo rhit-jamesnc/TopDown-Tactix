63e5ab26b25666a61ad310c3fb07cdb09e08c1bb
rhit-jamesnc <jamesnc@rose-hulman.edu>
2026-07-08 15:06:46 -0400

    Fixed Backend URL For Fetch Live Stats

--- client/src/components/Shared/utils/api.ts
+++ client/src/components/Shared/utils/api.ts
@@ -1,8 +1,10 @@
 import type { GameStats } from "../../../../../shared/types/props";
 
+const API_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000';
+
 export const fetchLiveStats = async (): Promise<GameStats> => {
     const start = performance.now();
-    const response = await fetch('/api/stats');
+    const response = await fetch(`${API_URL}/api/stats`);
     const end = performance.now();
     
     if (!response.ok) {
@@ -10,7 +12,7 @@ export const fetchLiveStats = async (): Promise<GameStats> => {
     }
 
     const data = await response.json();
-    
+
     data.server.ping = Math.round(end - start);
     
     return data;
